03bb295357319c00fc6d9936484f8356f7e61f3c
Brandon Hughes <157261373+thehundo@users.noreply.github.com>
2024-03-21 17:13:08 -0400

    Solved solution with For loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const chars = text.length;
+    let reverseText = "";
+    console.log(chars);
+    for (let i = chars; i >= 0; i--) {
+        reverseText += text.charAt(i);
+    }
+    return reverseText;
 };
 
 // Do not edit below this line
